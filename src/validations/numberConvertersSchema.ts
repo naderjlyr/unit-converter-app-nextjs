22dd1fded735e8b35ea9090bde2ea887d816a4bf
nader <naderjlyr@gmail.com>
2024-03-06 01:44:42 +0100

    validation message fix

--- src/validations/numberConvertersSchema.ts
+++ src/validations/numberConvertersSchema.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 
 export const decimalToRomanSchema = z.object({
-  inputValue: z.string().regex(/^\d+$/, "Input must be a non-negative integer"),
+  inputValue: z
+    .string()
+    .regex(
+      /^\d+$/,
+      "Input must be a positive number or zero, no other characters are accepted"
+    ),
 });
 
 export const binaryToRomanSchema = z.object({
